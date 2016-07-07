4e7283ab97e45c21db71fb5e9da9ae38a4f81080
HemaSri <hemasri@PickZys-Mac-mini.local>
2016-07-07 18:39:32 +0530

    StringManipulationInC.xcodeproj

---
+++ StringManipulationInC/main.c
@@ -0,0 +1,50 @@
+//
+//  main.c
+//  StringManipulationInC
+//
+//  Created by HemaSri on 7/4/16.
+//  Copyright © 2016 HemaSri. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+
+    char givenString[30], outputString[30];
+    
+    printf("Please Enter a String => ");
+    scanf("%s",givenString);
+    
+    for (int i = 0; i<strlen(givenString); i++) {
+        
+        if (strlen(outputString)<1) {
+            
+            if ( !strcmp(&givenString[i], "_")) {
+                
+                strcpy(&outputString[i], &givenString[i]);
+            }
+        
+            printf("%c",outputString[i]);
+        }
+        else {
+            
+            if (strcmp(&givenString[i-1], &givenString[i])) {
+                
+                strcpy(&outputString[i], &givenString[i]);
+                strncat(&outputString[i-1], &outputString[i], 1);
+            }
+        }
+    }
+    for (unsigned int i = 0; i<strlen(outputString); i++) {
+        
+        if (!strcmp(&outputString[strlen(outputString)-1], " ") || !strcmp(&outputString[strlen(outputString)-1], "_")) {
+            
+            outputString[strlen(outputString)-1] = '\0';
+        }
+    }
+    
+    printf("%s",outputString);
+    
+    return 0;
+}
